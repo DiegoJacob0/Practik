37ed1de7ff829aff48c76dacc7db059aabb39359
DiegoJacob0 <71044611+DiegoJacob0@users.noreply.github.com>
2021-03-14 16:32:45 -0600

    Add files via upload

---
+++ collaborativework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace collaborativework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
